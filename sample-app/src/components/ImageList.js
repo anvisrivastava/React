3eccb77f3e0aeb9d7658d4ddab84624e5c719e9c
anvisrivastava <152413612+anvisrivastava@users.noreply.github.com>
2023-12-25 14:42:37 +0530

    Add files via upload

---
+++ sample-app/src/components/ImageList.js
@@ -0,0 +1,9 @@
+// import "./ImageList.css"
+import ImageList from "./ImageShow";
+function ImageLis(){
+    // const imagesFromApi = images.map((image) => {
+    //     return <ImageShow image = {image} />
+    // })
+    // return <div className="image-list">{imagesFromApi}</div>
+}
+export default ImageList;
